e61e6bccde5a952f777ff186fbac75dd9a6a3d63
alejandrofernandezmarti <alefermar16@alu.edu.gva.es>
2023-10-17 17:27:48 +0200

    s

--- php/src/index.php
+++ php/src/index.php
@@ -1,6 +1,30 @@
 <?php
-$book =  new Book(1, 'MOD101', 'Publisher', 29.99, 200, 'Available', 'image.jpg', 'Great book!');
-$user = new User("alex@alex","password","nick");
-echo $user;
-echo $book
-?>
+include("autoload.php");
+use BatBook\Book;
+use BatBook\Course;
+use BatBook\Exempcions\InvalidFormatException;
+use BatBook\Module;
+use BatBook\User;
+
+$courses = array();
+$books = array();
+$modules = array();
+$users = array();
+
+try {
+    $courses= Course::loadCoursesFromFile('./files/coursesbook.csv');
+    $modules = Module::loadModulesFromFile('./files/modulesbook.csv');
+} catch (InvalidFormatException $e) {
+    throw new InvalidArgumentException($e);
+}
+
+
+$users[] = new User("alex@gmail.com","asdfghjK1","ThisNick");
+$books[] = new Book(1,"0011","Barco de vapor",15.00,330,"new","noPhoto","libro");
+
+foreach ($users as $key => $value){
+    echo $value;
+}
+foreach ($books as $key => $value){
+    echo $value;
+}
